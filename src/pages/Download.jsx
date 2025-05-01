9f2449f8c9fc6f0b0545fa23331c5f7a92b4c2ae
Matei Dragutu <67979189+DragutuMatei@users.noreply.github.com>
2025-05-01 16:57:20 +0300

    edit LevelUp path

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -5,13 +5,13 @@ function Download() {
     useEffect(()=>{
         var link = document.createElement("a");
         link.download = 'name';
-        link.href = require("../../assets/levelup/bafta.png");
+        link.href = require("../assets/levelup/bafta.png");
         link.click();
         link.remove();
     },[])
 
   return (
-    <a href={require("../../assets/levelup/bafta.png")} download={true}>Daca nu s a downloadad imaginea, apasa aici!</a>
+    <a href={require("../assets/levelup/bafta.png")} download={true}>Daca nu s a downloadad imaginea, apasa aici!</a>
   )
 }
 

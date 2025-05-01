c1bc765b7a4d32bdfc8740a3c9f8c919f2ac6a7a
Matei Dragutu <67979189+DragutuMatei@users.noreply.github.com>
2025-05-01 17:16:04 +0300

    adsasda

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -1,50 +1,65 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 function Download() {
+  const { nr } = useParams();
 
-    useEffect(()=>{
-        var link = document.createElement("a");
-        link.download = 'bafta';
-        link.href = require("../assets/levelup/bafta.txt");
-        link.click();
-        link.remove();
-    },[])
+  useEffect(() => {
+    var link = document.createElement("a");
+    if (nr == 1) {
+      link.download = "bafta";
+      link.href = require("../assets/levelup/bafta.txt");
+    } else {
+      link.download = "cauta_numele.pdf";
+      link.href = require("../assets/levelup/cauta numele.pdf");
+    }
+    link.click();
+    link.remove();
+  }, []);
 
   return (
     <>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <a className="button" href={require("../assets/levelup/bafta.txt")} download={true}>Daca nu s a downloadad imaginea, apasa aici!</a>
-  
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-  </>)
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <a
+        className="button"
+        href={require(`../assets/levelup/${
+          nr == 1 ? "bafta.txt" : "cauta numele.pdf"
+        }`)}
+        download={true}
+      >
+        Daca nu s a downloadad imaginea, apasa aici!
+      </a>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
 }
 
-export default Download
+export default Download;

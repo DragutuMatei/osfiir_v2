49449321c58cf74f7ddfbd08fced45a73b0399e7
Matei Dragutu <67979189+DragutuMatei@users.noreply.github.com>
2025-09-02 23:43:39 +0300

    a

--- src/pages/Voluntariat/Card.jsx
+++ src/pages/Voluntariat/Card.jsx
@@ -3,18 +3,18 @@ import "./Card.scss";
 
 function Card({not}) {
   const documents = [
-    { name: "Contract de voluntariat", filePath: "public/Documente_necesare/OSFIIR/Contract de voluntariat.d0d9476c9cceade447fa (1).docx" },
-    { name: "Fisa de voluntariat", filePath: "public/Documente_necesare/OSFIIR/Fisa de voluntariat.d843c838a771f7704378.docx" },
-    { name: "Apreciere", filePath: "public/Documente_necesare/OSFIIR/Apreciere.docx" },
-    { name: "Raport de activitate", filePath: "public/Documente_necesare/OSFIIR/Raport de activitate.5c10c77a4ebf5bcaf927.docx" },
-    { name: "Fisa de protectie a voluntarului", filePath: "public/Documente_necesare/OSFIIR/Fişa de protecţie a voluntarului.eaf6b2c38ba415ce6e6a.docx" },
+    { name: "Contract de voluntariat", filePath: "/Documente_necesare/OSFIIR/Contract de voluntariat.d0d9476c9cceade447fa (1).docx" },
+    { name: "Fisa de voluntariat", filePath: "/Documente_necesare/OSFIIR/Fisa de voluntariat.d843c838a771f7704378.docx" },
+    { name: "Apreciere", filePath: "/Documente_necesare/OSFIIR/Apreciere.docx" },
+    { name: "Raport de activitate", filePath: "/Documente_necesare/OSFIIR/Raport de activitate.5c10c77a4ebf5bcaf927.docx" },
+    { name: "Fisa de protectie a voluntarului", filePath: "/Documente_necesare/OSFIIR/Fişa de protecţie a voluntarului.eaf6b2c38ba415ce6e6a.docx" },
   ];
  const documents1 = [
-    { name: "Contract de voluntariat", filePath: "public/Documente_necesare/OSFIIR/Contract de voluntariat.d0d9476c9cceade447fa (1).docx" },
-    { name: "Fisa de voluntariat", filePath: "public/Documente_necesare/OSFIIR/Fisa de voluntariat.d843c838a771f7704378.docx" },
-    { name: "Apreciere", filePath: "public/Documente_necesare/OSFIIR/Apreciere.docx" },
-    { name: "Raport de activitate", filePath: "public/Documente_necesare/OSFIIR/Raport de activitate.5c10c77a4ebf5bcaf927.docx" },
-    { name: "Fisa de protectie a voluntarului", filePath: "public/Documente_necesare/OSFIIR/Fişa de protecţie a voluntarului.eaf6b2c38ba415ce6e6a.docx" },
+    { name: "Contract de voluntariat", filePath: "/Documente_necesare/OSFIIR/Contract de voluntariat.d0d9476c9cceade447fa (1).docx" },
+    { name: "Fisa de voluntariat", filePath: "/Documente_necesare/OSFIIR/Fisa de voluntariat.d843c838a771f7704378.docx" },
+    { name: "Apreciere", filePath: "/Documente_necesare/OSFIIR/Apreciere.docx" },
+    { name: "Raport de activitate", filePath: "/Documente_necesare/OSFIIR/Raport de activitate.5c10c77a4ebf5bcaf927.docx" },
+    { name: "Fisa de protectie a voluntarului", filePath: "/Documente_necesare/OSFIIR/Fişa de protecţie a voluntarului.eaf6b2c38ba415ce6e6a.docx" },
   ];
   return (
     <div className="card">
